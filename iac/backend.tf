fb7ffaae354058d8035fb1f96728e4808caf04fe
Puneet Gavri <puneet.gavri@gmail.com>
2026-01-17 02:33:32 +0530

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "terraformstatebucketdemo"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile   = true
     encrypt        = true
   }
 }
